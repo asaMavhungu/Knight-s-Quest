bc092a14f7dcde898285986335794059e2b92348
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-04-03 19:35:41 +0200

    Fixed positioning and size updates plus misc

--- Scripts/piece.py
+++ Scripts/piece.py
@@ -11,16 +11,22 @@ class Piece():
         
 
     def scale_by(self, factor: int) -> None:
+        old_rect = self.rect.copy()
         self.image = pygame.transform.scale_by(self.image, factor)
-        self.rect.size = self.image.get_size()
+        #self.rect.size = self.image.get_size()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = old_rect.topleft
+        print(self.rect.x)
 
-    def move(self, pos) -> None:
-        self.pos = pos
+    def move(self, x, y) -> None:
+        self.pos = (x, y)
+        self.rect.x, self.rect.y = x, y
 
 
 class Pawn(Piece):
     def __init__(self, pos: tuple) -> None:
         super().__init__("./Assets/pawn.png")
-        self.move(pos)
+        self.move(pos[0], pos[1])
+        print(pos)
 
 
